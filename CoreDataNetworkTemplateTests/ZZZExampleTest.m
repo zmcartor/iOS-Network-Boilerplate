070178b2fbb388e2c5ee2a95f8893cdd8ae21bfc
Zach <zmcartor@gmail.com>
2013-10-07 17:47:17 -0400

    First test placeholder

---
+++ CoreDataNetworkTemplateTests/ZZZExampleTest.m
@@ -0,0 +1,18 @@
+//
+//  ZZZExampleTest.m
+//  CoreDataNetworkTemplate
+//
+//  Created by zm on 10/7/13.
+//  Copyright (c) 2013 Hackazach. All rights reserved.
+//
+
+
+#import "Kiwi.h"
+
+SPEC_BEGIN(COOLSPEC)
+
+describe(@"test spec", ^{
+    // specs go here
+});
+
+SPEC_END
